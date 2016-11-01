7cd2990a638fffd179bb6157ae3b38bf8cbb691f
Onur EKER <onur.eker@gmail.com>
2016-11-01 21:26:33 +0300

    TestRunner is added
    TestRunner is added

---
+++ Source/Projects/Bosphorus.Assemble.BootStrapper/Runner.Test/TestRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Bosphorus.Common.Api.Symbol;
+using Bosphorus.Common.Application;
+using Castle.Core.Internal;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Environment = Bosphorus.Common.Application.Environment;
+
+namespace Bosphorus.Assemble.BootStrapper.Runner.Test
+{
+    public class TestRunner
+    {
+        private static readonly ITypeProvider typeProvider;
+
+        static TestRunner()
+        {
+            AssemblyFilter assemblyFilter = new AssemblyFilter(".");
+            IEnumerable<Assembly> assemblies = ReflectionUtil.GetAssemblies(assemblyFilter);
+            IEnumerable<Type> types = assemblies.SelectMany(assembly => assembly.GetTypes());
+            typeProvider = new DefaultTypeProvider(types);
+        }
+
+        public static IWindsorContainer Run(Environment environment, Perspective perspective, params Type[] installerTypes)
+        {
+            IoC ioc = new IoC(typeProvider, environment, perspective, Host.Console, installerTypes);
+            var windsorContainer = ioc.Resolve<IWindsorContainer>();
+            return windsorContainer;
+        }
+
+    }
+}
